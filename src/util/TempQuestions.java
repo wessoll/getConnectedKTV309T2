c3ee6476780dd1b697540a083ff721a87af79af6
Menno <mennomen@gmail.com>
2013-04-23 12:45:48 +0200

    tempquestions
    je weet zelluf

--- src/util/TempQuestions.java
+++ src/util/TempQuestions.java
@@ -24,7 +24,7 @@ public class TempQuestions {
         antwoorden.add(new Answer(400,100,false,"D"));
         
         Question question = new Question("Waar ligt het hoogste punt van Nederland?",
-                ImageUtil.getAndLoadImageIcon(Main.IMAGES_LOCATION + "vraag_nederland_1.jpg"),
+                ImageUtil.getAndLoadImageIcon(Main.IMAGES_LOCATION + "hoogtekaart.gif"),
                 antwoorden);
         return question;
     }
@@ -36,11 +36,130 @@ public class TempQuestions {
         antwoorden.add(new Answer(100,450,false,"C"));
         antwoorden.add(new Answer(450,250,false,"D"));
         
-        Question question = new Question("In welke provincie lopen de temperaturen het meest uiteen?",
-                ImageUtil.getAndLoadImageIcon(Main.IMAGES_LOCATION + "vraag_nederland_2.png"),
+        Question question = new Question("Waar ligt het het laagste punt van Nederland?",
+                ImageUtil.getAndLoadImageIcon(Main.IMAGES_LOCATION + "hoogtekaart.gif"),
+                antwoorden);
+        return question;
+        }
+       
+        public static Question getQuestion3() throws Exception{
+        ArrayList<Answer> antwoorden = new ArrayList<Answer>();
+    
+        antwoorden.add(new Answer(380,480,false,"A"));
+        antwoorden.add(new Answer(220,200,true,"B"));
+        antwoorden.add(new Answer(100,450,false,"C"));
+        antwoorden.add(new Answer(450,250,false,"D"));
+        
+        Question question = new Question("Waar ligt de snelweg A1?",
+                ImageUtil.getAndLoadImageIcon(Main.IMAGES_LOCATION + "snelwegenkaart.jpg"),
                 antwoorden);
         return question;
     }
+        public static Question getQuestion4() throws Exception{
+        ArrayList<Answer> antwoorden = new ArrayList<Answer>();
+    
+        antwoorden.add(new Answer(380,480,false,"A"));
+        antwoorden.add(new Answer(220,200,true,"B"));
+        antwoorden.add(new Answer(100,450,false,"C"));
+        antwoorden.add(new Answer(450,250,false,"D"));
+        
+        Question question = new Question("Waar ligt de snelweg A2?",
+                ImageUtil.getAndLoadImageIcon(Main.IMAGES_LOCATION + "snelwegenkaart.jpg"),
+                antwoorden);
+        return question;
+    }
+        public static Question getQuestion5() throws Exception{
+        ArrayList<Answer> antwoorden = new ArrayList<Answer>();
+    
+        antwoorden.add(new Answer(380,480,false,"A"));
+        antwoorden.add(new Answer(220,200,true,"B"));
+        antwoorden.add(new Answer(100,450,false,"C"));
+        antwoorden.add(new Answer(450,250,false,"D"));
+        
+        Question question = new Question("In welke provincie ligt de hoofdstad van Nederland",
+                ImageUtil.getAndLoadImageIcon(Main.IMAGES_LOCATION + "provincies.jpg"),
+                antwoorden);
+        return question;
+    }
+        public static Question getQuestion6() throws Exception{
+        ArrayList<Answer> antwoorden = new ArrayList<Answer>();
+    
+        antwoorden.add(new Answer(380,480,false,"A"));
+        antwoorden.add(new Answer(220,200,true,"B"));
+        antwoorden.add(new Answer(100,450,false,"C"));
+        antwoorden.add(new Answer(450,250,false,"D"));
+        
+        Question question = new Question("Waar ligt het warmste gebied in Nederland?",
+                ImageUtil.getAndLoadImageIcon(Main.IMAGES_LOCATION + "temperatuurkaart.png"),
+                antwoorden);
+        return question;
+    }
+        public static Question getQuestion7() throws Exception{
+        ArrayList<Answer> antwoorden = new ArrayList<Answer>();
+    
+        antwoorden.add(new Answer(380,480,false,"A"));
+        antwoorden.add(new Answer(220,200,true,"B"));
+        antwoorden.add(new Answer(100,450,false,"C"));
+        antwoorden.add(new Answer(450,250,false,"D"));
+        
+        Question question = new Question("Waar ligt het koudste gebied in Nederland?",
+                ImageUtil.getAndLoadImageIcon(Main.IMAGES_LOCATION + "temperatuurkaart.png"),
+                antwoorden);
+        return question;
+    }
+        public static Question getQuestion8() throws Exception{
+        ArrayList<Answer> antwoorden = new ArrayList<Answer>();
+    
+        antwoorden.add(new Answer(380,480,false,"A"));
+        antwoorden.add(new Answer(220,200,true,"B"));
+        antwoorden.add(new Answer(100,450,false,"C"));
+        antwoorden.add(new Answer(450,250,false,"D"));
+        
+        Question question = new Question("In welk gebied in Nederland waait het het hardst?",
+                ImageUtil.getAndLoadImageIcon(Main.IMAGES_LOCATION + "windkaart.png"),
+                antwoorden);
+        return question;
+    }
+        public static Question getQuestion9() throws Exception{
+        ArrayList<Answer> antwoorden = new ArrayList<Answer>();
+    
+        antwoorden.add(new Answer(380,480,false,"A"));
+        antwoorden.add(new Answer(220,200,true,"B"));
+        antwoorden.add(new Answer(100,450,false,"C"));
+        antwoorden.add(new Answer(450,250,false,"D"));
+        
+        Question question = new Question("In welk gebied in Nederland waait het het zachtst?",
+                ImageUtil.getAndLoadImageIcon(Main.IMAGES_LOCATION + "windkaart.png"),
+                antwoorden);
+        return question;
+    }
+        public static Question getQuestion10() throws Exception{
+        ArrayList<Answer> antwoorden = new ArrayList<Answer>();
+    
+        antwoorden.add(new Answer(380,480,false,"A"));
+        antwoorden.add(new Answer(220,200,true,"B"));
+        antwoorden.add(new Answer(100,450,false,"C"));
+        antwoorden.add(new Answer(450,250,false,"D"));
+        
+        Question question = new Question("In welk gebied is de meeste neerslag gevallen?",
+                ImageUtil.getAndLoadImageIcon(Main.IMAGES_LOCATION + "neerslag.png"),
+                antwoorden);
+        return question;
+    }
+        public static Question getQuestion11() throws Exception{
+        ArrayList<Answer> antwoorden = new ArrayList<Answer>();
+    
+        antwoorden.add(new Answer(380,480,false,"A"));
+        antwoorden.add(new Answer(220,200,true,"B"));
+        antwoorden.add(new Answer(100,450,false,"C"));
+        antwoorden.add(new Answer(450,250,false,"D"));
+        
+        Question question = new Question("In welk gebied is de minste neerslag gevallen?",
+                ImageUtil.getAndLoadImageIcon(Main.IMAGES_LOCATION + "neerslag.png"),
+                antwoorden);
+        return question;
+    }
+        
     
     
     
